9a33c40b88fa6b7cd1c836d7720d492866fc7245
yarnaid <yarnaid@gmail.com>
2025-12-14 13:43:19 +0400

    aerospace

--- aerospace.nix
+++ aerospace.nix
@@ -74,8 +74,18 @@
           "alt-7" = "workspace 7";
           "alt-8" = "workspace 8";
           "alt-9" = "workspace 9";
+          "alt-q" = "workspace Q";
           "alt-w" = "workspace W";
           "alt-e" = "workspace E";
+          "alt-r" = "workspace R";
+          "alt-a" = "workspace A";
+          "alt-s" = "workspace S";
+          "alt-d" = "workspace D";
+          "alt-f" = "workspace F";
+          "alt-z" = "workspace Z";
+          "alt-x" = "workspace X";
+          "alt-c" = "workspace C";
+          "alt-v" = "workspace V";
           
           # Move to workspace
           "alt-shift-1" = "move-node-to-workspace 1";
@@ -87,8 +97,18 @@
           "alt-shift-7" = "move-node-to-workspace 7";
           "alt-shift-8" = "move-node-to-workspace 8";
           "alt-shift-9" = "move-node-to-workspace 9";
+          "alt-shift-q" = "move-node-to-workspace Q";
           "alt-shift-w" = "move-node-to-workspace W";
           "alt-shift-e" = "move-node-to-workspace E";
+          "alt-shift-r" = "move-node-to-workspace R";
+          "alt-shift-a" = "move-node-to-workspace A";
+          "alt-shift-s" = "move-node-to-workspace S";
+          "alt-shift-d" = "move-node-to-workspace D";
+          "alt-shift-f" = "move-node-to-workspace F";
+          "alt-shift-z" = "move-node-to-workspace Z";
+          "alt-shift-x" = "move-node-to-workspace X";
+          "alt-shift-c" = "move-node-to-workspace C";
+          "alt-shift-v" = "move-node-to-workspace V";
           
           # Other controls
           "alt-tab" = "workspace-back-and-forth";
@@ -112,12 +132,146 @@
       };
 
       workspace-to-monitor-force-assignment = {
-        "W" = "Built-in Retina Display";
-        "E" = "TYPE-C";
+        # "W" = "Built-in Retina Display";
+        # "E" = "TYPE-C";
       };
 
       # Window detection rules
       on-window-detected = [
+      {
+        "if" = {
+          app-id = "com.kagi.kagimacOS";
+        };
+        run = "move-node-to-workspace Q";
+      }
+      {
+        "if" = {
+          app-id = "company.thebrowser.Browser";
+        };
+        run = "move-node-to-workspace Q";
+      }
+
+      {
+        "if" = {
+          app-id = "ch.protonmail.desktop";
+        };
+        run = "move-node-to-workspace W";
+      }
+      {
+        "if" = {
+          app-id = "io.canarymail.mac";
+        };
+        run = "move-node-to-workspace W";
+      }
+      {
+        "if" = {
+          app-id = "com.microsoft.Outlook";
+        };
+        run = "move-node-to-workspace W";
+      }
+
+
+      {
+        "if" = {
+          app-id = "com.microsoft.teams2";
+        };
+        run = "move-node-to-workspace E";
+      }
+      {
+        "if" = {
+          app-id = "ru.keepcoder.Telegram";
+        };
+        run = "move-node-to-workspace E";
+      }
+
+      {
+        "if" = {
+          app-id = "io.readwise.read";
+        };
+        run = "move-node-to-workspace R";
+      }
+
+
+      {
+        "if" = {
+          app-id = "com.todesktop.230313mzl4w4u92";
+        };
+        run = "move-node-to-workspace A";
+      }
+      {
+        "if" = {
+          app-id = "com.microsoft.VSCode";
+        };
+        run = "move-node-to-workspace A";
+      }
+      {
+        "if" = {
+          app-id = "com.google.antigravity";
+        };
+        run = "move-node-to-workspace A";
+      }
+
+
+      {
+        "if" = {
+          app-id = "com.mitchellh.ghostty";
+        };
+        run = "move-node-to-workspace S";
+      }
+      {
+        "if" = {
+          app-id = "com.googlecode.iterm2";
+        };
+        run = "move-node-to-workspace S";
+      }
+      {
+        "if" = {
+          app-id = "com.apple.ActivityMonitor";
+        };
+        run = "move-node-to-workspace D";
+      }
+
+
+
+      {
+        "if" = {
+          app-id = "inc.tana.desktop";
+        };
+        run = "move-node-to-workspace Z";
+      }
+      {
+        "if" = {
+          app-id = "com.culturedcode.ThingsMac";
+        };
+        run = "move-node-to-workspace Z";
+      }
+
+      {
+        "if" = {
+          app-id = "ru.yandex.desktop.music";
+        };
+        run = "move-node-to-workspace X";
+      }
+      {
+        "if" = {
+          app-id = "com.spotify.client";
+        };
+        run = "move-node-to-workspace X";
+      }
+      {
+        "if" = {
+          app-id = "com.apple.Music";
+        };
+        run = "move-node-to-workspace X";
+      }
+
+
+      {
+        "if" = {
+          app-id = "com.microsoft.rdc.macos";
+        };
+        run = "move-node-to-workspace C";
+      }
         # {
         #   "if" = {
         #     app-id = "com.apple.Safari";
