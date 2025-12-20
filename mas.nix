54b51c0cb5a6e8573f7b52439bf7fe57586a1646
yarnaid <yarnaid@gmail.com>
2025-12-20 19:01:42 +0400

    update

--- mas.nix
+++ mas.nix
@@ -6,59 +6,58 @@
     "Microsoft Word" = 462054704;
     "Microsoft PowerPoint" = 462062816;
     "Microsoft Outlook" = 985367838;
-    "Actions" = 1586435171;
     "Pages" = 409201541;
     "Numbers" = 409203825;
     "Keynote" = 409183694;
     "Soulver 3" = 1508732804;
     "LiquidText" = 922765270;
-    "Merlin Project" = 1335639877;
+    # "Merlin Project" = 1335639877;
     "Merlin Project Express" = 1202196869;
-    "SnippetsLab" = 1006087419;
-    "Tot" = 1491071483;
-    "LaunchPalette" = 6474854386;
+    # "SnippetsLab" = 1006087419;
+    # "Tot" = 1491071483;
+    # "LaunchPalette" = 6474854386;
 
     # Email & Communication
     "Canary Mail" = 1236045954;
 
     # Reading & Reference
-    "ReadKit" = 1615798039;
-    "Reeder" = 6475002485;
-    "GoodLinks" = 1474335294;
+    # "ReadKit" = 1615798039;
+    # "Reeder" = 6475002485;
+    # "GoodLinks" = 1474335294;
 
     # Utilities
-    "Disk Space Analyzer" = 446243721;
+    # "Disk Space Analyzer" = 446243721;
     "Blackmagic Disk Speed Test" = 425264550;
     "Apple Configurator" = 1037126344;
     "Amphetamine" = 937984704;
     "OneDrive" = 823766827;
-    "Parcel Classic" = 639968404;
+    # "Parcel Classic" = 639968404;
     "Key Codes" = 414568915;
     "WiFi Explorer" = 494803304;
     "WiFi Signal" = 525912054;
     "Windows App" = 1295203466;
     "Speedtest" = 1153157709;
-    "Any Screen Resolution" = 6444128384;
-    "BLEScanner" = 1663446245;
+    # "Any Screen Resolution" = 6444128384;
+    # "BLEScanner" = 1663446245;
     "The Unarchiver" = 425424353;
     "RAR Extractor - Unarchiver" = 646295438;
     "Unzip - RAR ZIP 7Z Unarchiver" = 1537056818;
-    "Posture Pals" = 1590316152;
-    "Parachute" = 6748614170;
+    # "Posture Pals" = 1590316152;
+    # "Parachute" = 6748614170;
 
     # Development & Power User Tools
     "JSON Peep" = 1458969831;
     "Peek" = 1554235898;
     "Super Agent" = 1568262835;
     "Xcode" = 497799835;
-    "rcmd" = 1596283165;
-    "Expressions" = 913158085;
+    # "rcmd" = 1596283165;
+    # "Expressions" = 913158085;
 
     # Browser Extensions & Web Tools
     "Vimlike" = 1584519802;
     "StopTheMadness Pro" = 6471380298;
     "PayPal Honey" = 1472777122;
-    "1Password for Safari" = 1569813296;
+    # "1Password for Safari" = 1569813296;
     "Kagi for Safari" = 1622835804;
     "PiPifier" = 1160374471;
     "LanguageTool" = 1534275760;
@@ -66,8 +65,8 @@
     "Karma" = 1481191441;
     "Keyword Search" = 1558453954;
     "Fakespot" = 1592541616;
-    "Fello AI" = 6447705369;
-    "Perplexity" = 6714467650;
+    # "Fello AI" = 6447705369;
+    # "Perplexity" = 6714467650;
     "Hush" = 1544743900;
 
     # Network & System Tools
@@ -78,18 +77,18 @@
     # Media & Graphics
     "Pixelmator Pro" = 1289583905;
     "YamaCast" = 1415107621;
-    "FilmNoir" = 1528417240;
+    # "FilmNoir" = 1528417240;
     "Unsplash Wallpapers" = 1284863847;
 
     # Finance
-    "Exchange Rates" = 1463361068;
+    # "Exchange Rates" = 1463361068;
 
     # Weather & Lifestyle
-    "CARROTweather" = 993487541;
-    "Drafts" = 1435957248;
+    # "CARROTweather" = 993487541;
+    # "Drafts" = 1435957248;
     "flowy" = 6748351905;
-    "Mic Drop" = 1489816366;
-    "Micro Snitch" = 972028355;
+    # "Mic Drop" = 1489816366;
+    # "Micro Snitch" = 972028355;
     "Save to Raindrop.io" = 1549370672;
     "Save to Reader" = 1640236961;
     "Anybox" = 1593408455;
